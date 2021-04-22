26fd645a13cbe8ce7d15ab1ffc10e52700b79f60
arnav gupta <c-arnav.gupta@charter.com>
2021-04-22 15:33:36 -0600

    fourth commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,4 @@
-INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
-  ('Lokesh', 'Gupta', 'abc@gmail.com'),
-  ('Deja', 'Vu', 'xyz@email.com'),
-  ('Caption', 'America', 'cap@marvel.com');
-  
+
   INSERT INTO pet (name, type, price) VALUES
   ('Max', 'Dog1', '15'),
   ('Tommy', 'Dog2', '20'),
